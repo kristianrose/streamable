f8d69351e43b65d0eb6e20cbda681ffc8bab37e6
Kristian Rose <62854656+kristianrose@users.noreply.github.com>
2024-03-21 22:20:01 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ const Footer: React.FC = () => {
     return (
       <footer className="bg-[#161822] text-cyan-300 font-light p-9 pt-20">
         <div className="container mx-auto">
-        <div className=" mx-auto flex place-content-center pb-4 "><Megaphone size={30} className="mr-2" /> <p>Streamable does not host any files, it merely links to 3rd party services.</p> </div>
+        <div className=" mx-auto flex place-content-center pb-4 "><Megaphone size={30} className="mr-2" /> <p>Mgrum does not host any files, it merely links to 3rd party services.</p> </div>
 
           <p className="text-center">
-            &copy; {new Date().getFullYear()} Streamable.
+            &copy; {new Date().getFullYear()} Ngrim
           </p>
         </div>
       </footer>
